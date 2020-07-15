d98c23d4ceb179b4d1d25302896107d0d2512d75
Jesus Gallardo <jjgalardior@gmail.com>
2020-07-15 04:20:22 -0430

    Gallery Finished

--- js/app.js
+++ js/app.js
@@ -29,8 +29,63 @@ menuList.forEach(element => {
     })
     element.classList.toggle('active')
 
+    // Cerrar menu al hacer click en un item
+    menu.style.top = "-900px";
+    menuIcon.classList.toggle('fa-times')
+
   })
 
 })
 
 // Scroll Spy
+
+
+const navbar = document.querySelector('.menu');
+console.log(navbar)
+const scrollspy = new VanillaScrollspy(navbar);
+scrollspy.init();
+
+// Galeria de Proyectos
+
+  // Abrir IMAGEN
+let gallery = document.querySelectorAll('.projects__gallery img')
+let modal = document.getElementById('modal')
+let modalImg = document.querySelector('.modal img')
+let btnNext = document.querySelector('.modal__next')
+let btnPrev = document.querySelector('.modal__prev')
+
+  // ABRIR IMAGEN
+for (let i = 0; i < gallery.length; i++) {
+  gallery[i].addEventListener("click", () => {
+    // Abrir imagen
+    modal.style.left = "0";
+    modal.style.background = "rgba(0,0,0, .7)";
+    modalImg.setAttribute('src', gallery[i].src);
+
+    // Boton Next
+    btnNext.addEventListener("click", () => {
+      if(gallery[i].id === "final"){
+        i = -1;
+      }
+      i++
+      modalImg.setAttribute('src', gallery[i].src);
+    })
+    // Boton Prev
+    btnPrev.addEventListener("click", () => {
+      if(gallery[i].id === "inicio"){
+        i = 10;
+      }
+      i--
+      modalImg.setAttribute('src', gallery[i].src);
+    })
+  })
+
+}
+
+  // Salir de la IMAGEN
+let btnExit = document.querySelector('.modal__btn')
+
+btnExit.addEventListener('click', () => {
+    modal.style.left = "-100%";
+    modal.style.background = "transparent";
+})
